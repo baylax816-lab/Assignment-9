fe4aa53004f7f29f1a5b3eba5896aeb1829a9788
baylax816-lab <baylax816@gmail.com>
2026-03-29 12:47:18 -0500

    I added to the script.js file to fetch a random gif from the Giphy API and display it on the page when the "Fetch a GIF" button is clicked.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
 console.log("script.js loaded");
+
+const apikey ="j9ZZx0Ufc8mFXI8OaJPJobV8VLDLETna";
+
+const gitContainer = document.querySelector("#gif-container");
+const fetchGifBtn = document.querySelector("#fetch-gif-btn");
+const input = document.querySelector("#search-input");
+
+let images = [];
+
+async function getGifs(searchTerm) {
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${searchTerm}&limit=12`;
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    images = data.data.map(gif => gif.images.original.url);
+    console.log(images);
+    displayGifs();
+}
+
+function displayGifs() {
+    gitContainer.innerHTML = "";
+    images.forEach(url => {
+        const col = document.createElement("div");
+        col.classList.add("col-md-4", "mb-4");
+        const img = document.createElement("img");
+        img.src = url;
+        img.classList.add("img-fluid");
+        col.appendChild(img);
+        gitContainer.appendChild(col);
+    });
+}
+
+fetchGifBtn.addEventListener("click", () => {
+    const searchTerm = input.value.trim();
+    if (searchTerm) {
+        getGifs(searchTerm);
+    }
+});
